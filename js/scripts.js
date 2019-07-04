48e3606390ee0f990f9cdd4ec7bf123db316f6f5
Craig Hutler and Mike McShane <craig@>
2019-07-03 19:55:57 -0700

    created object that contains two arrays

--- js/scripts.js
+++ js/scripts.js
@@ -1,31 +1,40 @@
 // Biznezz in the back
+function Transportation(fun, work) {
+  this.fun = fun,
+  this.work = work
+};
 
-
- // UI in the front
+// UI in the front
 $(document).ready(function(){
   $("form#transportation_survey").submit(function(event){
     event.preventDefault();
-    // $("#work-responses").show();
-    // $("input:checkbox[name=work-transportation]:checked").each(function(){
-    //   var workTransportationMode = $(this).val();
-    //   $('#work-responses').append(workTransportationMode + "<br>");
-    // });
-    // $("#fun-responses").show();
-    // $("input:checkbox[name=fun-transportation]:checked").each(function(){
-    //   var funTransportationMode = $(this).val();
-    //   $('#fun-responses').append(funTransportationMode + "<br>");
-    // });
-    $('#transportation_survey').hide();
-    var userResponses = [];
-      $("input:checkbox[name=work-transportation]:checked").each(function(){
-        var workTransportationMode = $(this).val();
-        userResponses.push(workTransportationMode);
-
-  var userResponses = [];
+    $("#work-responses").show();
     $("input:checkbox[name=work-transportation]:checked").each(function(){
       var workTransportationMode = $(this).val();
-      userResponses.push(workTransportationMode);
+      $('#work-responses').append(workTransportationMode + "<br>");
     });
-  console.log(userResponses);
+    $("#fun-responses").show();
+    $("input:checkbox[name=fun-transportation]:checked").each(function(){
+      var funTransportationMode = $(this).val();
+      $('#fun-responses').append(funTransportationMode + "<br>");
+    });
+    $('#transportation_survey').hide();
+
+    var userWorkResponses = [];
+    $("input:checkbox[name=work-transportation]:checked").each(function(){
+      var workTransportationMode = $(this).val();
+      userWorkResponses.push(workTransportationMode);
+    });
+
+    var userFunResponses = [];
+    $("input:checkbox[name=fun-transportation]:checked").each(function(){
+      var funTransportationMode = $(this).val();
+      userFunResponses.push(funTransportationMode);
+    });
+    // console.log(userWorkResponses);
+    // console.log(userFunResponses);
+
+    var newTransport = new Transportation(userFunResponses, userWorkResponses);
+    console.log(newTransport);
   });
 });
